c075b7d78d43d64587a1fae10e8b66bf48f7a7ee
Matija <31419257+MatijaBregar@users.noreply.github.com>
2023-02-05 20:08:50 +0100

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module weatherAPI
+
+go 1.20
+
+require github.com/briandowns/openweathermap v0.19.0 // indirect
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"encoding/json"
+	"net/http"
+	"io/ioutil"
+	"fmt"
+)
+
+type WeatherData struct {
+	City string `json:"city"`
+	Info string `json:"info"`
+}
+
+var apiKey = os.Getenv("bc94089fef9d46968ff8d724889f9fff")
+
+func main() {
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := r.URL.Path[len("/weather/"):]
+
+		url := "https://weatherapi-com.p.rapidapi.com/current.json?q=" + city
+
+		req, _ := http.NewRequest("GET", url, nil)
+
+		req.Header.Add("X-RapidAPI-Key", "356fcc1b96msh6b0261faa77fcd2p101d17jsnbbb68ac024b7")
+		req.Header.Add("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
+
+		res, _ := http.DefaultClient.Do(req)
+
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+
+		weatherData := WeatherData{
+			City:    city,
+			Info:    string(body),
+		}
+		jsonData, _ := json.Marshal(weatherData)
+		ioutil.WriteFile("weather.json", jsonData, 0644)
+
+		data, err := ioutil.ReadFile("weather.json")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, string(data))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
