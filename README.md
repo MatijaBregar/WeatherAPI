2daf9628221057173a7159b1e2c444d47fae8e40
maofaka12 <maofaka123@gmail.com>
2023-02-19 20:25:51 +0100

    Redesigned whole API, changed README

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# task1
-Change
+<h1 align="center">
+  <br>
+  <img src="" width="200">
+  <br>
+  WeatherAPI
+  <br>
+</h1>
+
+<h4 align="center">Simple Weather API built in GO language.</h4>
+
+<p align="center">
+  <a href="#key-features">Key Features</a> •
+  <a href="#how-to-use">How To Use</a> •
+  <a href="#credits">Credits</a>
+</p>
+
+## Key Features
+
+* Simplified Information In JSON format
+    - <img src="" width="200">
+* Without Complex Integration
+    - Input desired town
+* Returned Error
+    - If it can't get information about weather it returns appropriate error
+  
+
+## How To Use
+
+To clone and run this application, you'll need [GIN](https://github.com/gin-gonic/gin) and [GO](https://go.dev/dl/) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/MatijaBregar/WeatherAPI.git
+
+# Go into the repository
+$ cd WeatherAPI
+
+# Run the app
+$ go run main.go
+```
+
+[Optional]
+For better and faster testing you can use [POSTMAN](https://www.postman.com).
+Use localhost:8080/{_city_} for HTTP GET request.
+
+## Credits
+
+This software uses the following technologies:
+
+- [GIN](https://github.com/gin-gonic/gin)
+- [GO](https://go.dev/dl/)
+- [WEATHER-API.COM](WeatherAPI.com)
--- main.go
+++ main.go
@@ -1,65 +1,83 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"os"
-	"time"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"encoding/json"
 )
 
 type WeatherData struct {
 	City string `json:"city"`
-	Info string `json:"info"`
+	Country string `json:"country"`
+	Temp_C float64 `json:"temp_c"`
+	Temp_F float64 `json:"temp_f"`
+	Wind_KPH float64 `json:"wind_kph"`
+	Wind_MPH float64 `json:"wind_mph"`
 }
 
-var apiKey = os.Getenv("bc94089fef9d46968ff8d724889f9fff")
+func getWeatherByCity(city string) (*WeatherData, error) {
+	url := "https://weatherapi-com.p.rapidapi.com/current.json?q=" + city
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-RapidAPI-Key", "356fcc1b96msh6b0261faa77fcd2p101d17jsnbbb68ac024b7")
+	req.Header.Add("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return nil, errors.New("Error getting weather data")
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	var data map[string]interface{}
+    err2 := json.Unmarshal([]byte(string(body)), &data)
+
+    if err2 != nil {
+        return nil, errors.New("Error getting weather data")
+    }
+
+	var weatherData WeatherData
+
+	if location, ok := data["location"].(map[string]interface{}); ok {
+		if current, ok := data["current"].(map[string]interface{}); ok {
+			weatherData := WeatherData{
+				City: location["name"].(string),
+				Country: location["country"].(string),
+				Temp_C: current["temp_c"].(float64),
+				Temp_F: current["temp_f"].(float64),
+				Wind_KPH: current["wind_kph"].(float64),
+				Wind_MPH: current["wind_mph"].(float64),
+			}
+			return &weatherData, nil
+		} else {
+			return nil, errors.New("Error getting weather data")
+		}
+	} else {
+		return nil, errors.New("Error getting weather data")
+	}
+
+	return &weatherData, nil
+}
+
+func getWeather(context *gin.Context) {
+	city := context.Param("city")
+	weatherData, err := getWeatherByCity(city)
+
+	if err != nil {
+		context.IndentedJSON(http.StatusNotFound, gin.H{"error": "Was not able to get weather data"})
+		return
+	}
+
+	context.IndentedJSON(http.StatusOK, weatherData)
+}
 
 func main() {
-	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
-		city := r.URL.Path[len("/weather/"):]
-
-		url := "https://weatherapi-com.p.rapidapi.com/current.json?q=" + city
-
-		req, _ := http.NewRequest("GET", url, nil)
-
-		req.Header.Add("X-RapidAPI-Key", "356fcc1b96msh6b0261faa77fcd2p101d17jsnbbb68ac024b7")
-		req.Header.Add("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
-
-		res, err := http.DefaultClient.Do(req)
-
-		if err != nil {
-			fmt.Println("HTTP call failed:", err)
-			return
-		}
-
-		defer res.Body.Close()
-		body, _ := ioutil.ReadAll(res.Body)
-
-		weatherData := WeatherData{
-			City: city,
-			Info: string(body),
-		}
-		jsonData, _ := json.Marshal(weatherData)
-		ioutil.WriteFile("weather.json", jsonData, 0600)
-
-		data, err := ioutil.ReadFile("weather.json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, string(data))
-	})
-
-	server := &http.Server{
-		Addr:              ":8080",
-		ReadHeaderTimeout: 3 * time.Second,
-	}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+	r := gin.Default()
+	r.GET("/:city", getWeather)
+	r.Run("localhost:8080")
 }
